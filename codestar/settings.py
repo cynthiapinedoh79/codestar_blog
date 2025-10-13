ba6a92cceca16b4dd210fdff97accb6ba4d59ff3
Cynthia Pinedo <cynthiapinedoh79@gmail.com>
2025-10-13 06:23:27 +0000

    Creating SECURE_KEY

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#5n29p6=(=6)aj^^i8rx$^_*($fso#gqv##5&*(kk^o+lbc8w#'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
--- env.py
+++ env.py
@@ -1,3 +1,6 @@
 import os
 os.environ.setdefault(
 	"DATABASE_URL", "postgresql://neondb_owner:npg_n8fpgBuj9ztx@ep-icy-glade-ag48efye.c-2.eu-central-1.aws.neon.tech/yeast_heat_plus_84553")
+
+os.environ.setdefault(
+	"SECRET_KEY", "im$6w#o+$hd8@6jyelz4p)#x885j4v6eiwb(ff9-h1xj3=4af6")
