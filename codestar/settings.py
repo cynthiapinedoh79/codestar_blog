bc31087cf82a97ec1013f88d02f07d1f7cfd1c44
Cynthia Pinedo <cynthiapinedoh79@gmail.com>
2025-10-13 06:58:33 +0000

    adding whole route to ALLOWED_HOSTS

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,8 +29,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com',
-                 '127.0.0.1']
+ALLOWED_HOSTS = ['8000-cynthiapine-codestarblo-zxdijq1zyax.ws-us121.gitpod.io',
+                 .herokuapp.com',
+                 '127.0.0.1',]
 
 
 # Application definition
