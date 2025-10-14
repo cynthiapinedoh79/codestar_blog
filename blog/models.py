8cf2c6e5531a5032b1a9ba851a440f3908e2ed7f
Cynthia Pinedo <cynthiapinedoh79@gmail.com>
2025-10-14 01:18:15 +0000

    compled post model in blog

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="blog_posts"
 )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -16,8 +16,18 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, 
+        related_name="commenter",
+        null=True, blank=True,) # <-- TEMPORARY to allow migration
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
