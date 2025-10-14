8f68cf619e5235d71dd44727eb78d7b3f91ee0ce
Cynthia Pinedo <cynthiapinedoh79@gmail.com>
2025-10-14 21:01:25 +0000

    adding filtersto my blog app views.py

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,8 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    # queryset = Post.objects.filter(author=4) # Filter posts by author with ID 1
+    # queryset = Post.objects.all() # Get all posts
+    # queryset = Post.objects.all().order_by('-created_on') # Get all posts ordered by creation date descending
+    queryset = Post.objects.filter(status=1) # Get all published posts, not drafts
     template_name = 'post_list.html'
