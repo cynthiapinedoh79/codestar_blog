53a4e32c0311b59b40ee5e66862216beba369e66
Cpinedo <cynthiapinedoh79@gmail.com>
2025-10-17 18:07:43 +0000

    added post_detail view

--- blog/views.py
+++ blog/views.py
@@ -12,27 +12,16 @@ class PostList(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6
 
-    def post_detail(request, slug):
-        """
-        Display an individual :model:`blog.Post`.
+# This function is defined outside the class
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
 
-        **Context**
-
-        ``post``
-            An instance of :model:`blog.Post`.
-
-        **Template:**
-
-        :template:`blog/post_detail.html`
-        """
-
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-
-        return render(
-            request,
-            "blog/post_detail.html",
-            {"post": post},
-        )
-
-    
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
